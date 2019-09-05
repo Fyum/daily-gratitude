a26adf80f67daf680236fa360afc1d7628291278
Fyum <fyulheri.dev@gmail.com>
2019-09-05 21:21:11 +0100

    remove modal

--- App.js
+++ App.js
@@ -38,26 +38,6 @@ export default function App() {
         data={[1, 2, 3, 4, 5]}
         dummyItems={data}
       />
-      <Modal
-        animationType="slide"
-        transparent={false}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-        }}>
-        <View style={{ marginTop: 22 }}>
-          <View>
-            <Text>Hello World!</Text>
-
-            <TouchableHighlight
-              onPress={() => {
-                setModalVisible(!modalVisible);
-              }}>
-              <Text>Hide Modal</Text>
-            </TouchableHighlight>
-          </View>
-        </View>
-      </Modal>
       <AddButton onPress={() => setModalVisible(!modalVisible)} />
     </View>
   );
