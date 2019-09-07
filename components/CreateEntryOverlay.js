17b0ca8d189ba9b4c36dd47f73d0cad4f7fba434
Fyum <fyulheri.dev@gmail.com>
2019-09-07 15:52:53 +0100

    add entry

--- components/CreateEntryOverlay.js
+++ components/CreateEntryOverlay.js
@@ -78,6 +78,4 @@ const containerInputStyle = {
   marginBottom: 30
 };
 
-
-
 export default CreateEntryOverlay;
---
+++ components/Entry.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Entry = ({
+
+}) => {
+
+
+  return; //TODO
+}
+
+export default Entry;
+
