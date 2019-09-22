6a0827735bec8657d43a7479ab15dd4e1fc54c83
Fyum <fyulheri.dev@gmail.com>
2019-09-22 20:23:46 +0100

    transparent + add gradient package

--- components/AddButton.js
+++ components/AddButton.js
@@ -19,7 +19,7 @@ const AddButton = ({
       containerStyle={containerStyle}
       iconStyle={iconStyle}
       onPress={onPress}
-      underlayColor='#FFFFFF00'
+      underlayColor='transparent'
     />
   )
 };
