e59d02efbc4aa5885d910822c0dabc051778e440
Fyum <fyulheri.dev@gmail.com>
2019-09-29 20:52:42 +0100

    remove console.log

--- data-storage/delete_entry.js
+++ data-storage/delete_entry.js
@@ -7,12 +7,10 @@ const removeFromDay = (dayEntries, entryId) => {
 };
 
 const deleteEntry = async (dayKey, entryId) => {
-  console.log('Attempting to delete', dayKey, dayKey);
   const dayEntries = await getItem(dayKey);
   console.log(dayEntries);
   const updatedDayEntries = removeFromDay(dayEntries, entryId);
   await setItem(dayKey, updatedDayEntries);
-  console.log('Delete successful');
 }
 
 export default deleteEntry;
