5c5aca2d07df8271bdb9517737df641358b6521c
Fyum <fyulheri.dev@gmail.com>
2019-10-13 15:10:39 +0100

    styling the menu items

--- components/MainApp.js
+++ components/MainApp.js
@@ -1,15 +1,25 @@
 
 import React from 'react';
 
-import {createAppContainer} from 'react-navigation';
-import {createDrawerNavigator} from 'react-navigation-drawer';
-import {createStackNavigator} from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createStackNavigator } from 'react-navigation-stack';
 import ViewEntries from './ViewEntries';
 import ViewSettings from './ViewSettings';
 import MainHeader from './MainHeader'
 
+import themeStyle from '../themes/styles';
+const {
+  headerBackgroundColor,
+  contentBackgroundColor,
+  textColor,
+} = themeStyle;
 
-const NavigationDrawerHeader = ({ navigationProps }) => 
+/**
+ * Documentation see : https://reactnavigation.org/docs/en
+ */
+
+const NavigationDrawerHeader = ({ navigationProps }) =>
   <MainHeader onClickMenu={navigationProps.toggleDrawer} />
 
 const StackNavigatorViewEntries = createStackNavigator({
@@ -33,19 +43,33 @@ const StackNavigatorViewSettings = createStackNavigator({
 });
 
 
-const DrawerNavigator = createDrawerNavigator({
-  //Drawer Optons and indexing
-  ViewEntries: {
-    screen: StackNavigatorViewEntries,
-    navigationOptions: {
-      drawerLabel: "Entries"
-    }
+const DrawerNavigator = createDrawerNavigator(
+  {
+    //Drawer Optons and indexing
+    ViewEntries: {
+      screen: StackNavigatorViewEntries,
+      navigationOptions: {
+        drawerLabel: "Entries"
+      }
+    },
+    ViewSettings: {
+      screen: StackNavigatorViewSettings,
+      navigationOptions: {
+        drawerLabel: "Settings"
+      }
+    },
   },
-  ViewSettings: { 
-    screen: StackNavigatorViewSettings,
-    navigationOptions: {
-      drawerLabel: "Settings"
+  {
+    // Drawer configuration
+    drawerBackgroundColor: '#29134A',
+    contentOptions: {
+      inactiveTintColor: textColor,
+      activeTintColor: '#ECD1D5',
+      activeBackgroundColor: '#AF656F',
+      itemStyle: {
+        paddingLeft:20,
+      }
     }
-  },
-});
+  }
+);
 export default createAppContainer(DrawerNavigator);
