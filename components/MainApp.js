8cd29d11e9f377868e92c5bdc6a33157fa6f2283
Fyum <fyulheri.dev@gmail.com>
2019-10-13 13:37:51 +0100

    add new component and use drawer

--- components/MainApp.js
+++ components/MainApp.js
@@ -1,15 +1,88 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 
+import { View } from 'react-native'
+
+import {createAppContainer} from 'react-navigation';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import {createStackNavigator} from 'react-navigation-stack';
 import ViewEntries from './ViewEntries';
+import ViewSettings from './ViewSettings';
+import MainHeader from './MainHeader'
 
 
-const MainApp = () =>{ 
 
-
-  return (
-    <ViewEntries />
-  )
+class NavigationDrawerStructure extends Component {
+  //Structure for the navigatin Drawer
+  toggleDrawer = () => {
+    //Props to open/close the drawer
+    this.props.navigationProps.toggleDrawer();
+  };
+  render() {
+    return (
+      <View style={{ flexDirection: 'row' }}>
+        <MainHeader
+           onClickMenu={this.toggleDrawer.bind(this)}
+        />
+      </View>
+    );
+  }
 }
 
-export default MainApp;
+const StackNavigatorViewEntries = createStackNavigator({
+  //All the screen from the Screen1 will be indexed here
+  First: {
+    screen: ViewEntries,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Demo Screen 1',
+      headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
+      headerStyle: {
+        backgroundColor: '#FFF',
+      },
+      headerTintColor: '#fff',
+    }),
+  },
+});
+
+const StackNavigatorViewSettings = createStackNavigator({
+  //All the screen from the Screen1 will be indexed here
+  Second: {
+    screen: ViewSettings,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Demo Screen 2',
+      headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
+      headerStyle: {
+        backgroundColor: '#FFF',
+      },
+      headerTintColor: '#fff',
+    }),
+  },
+});
+
+
+const DrawerNavigator = createDrawerNavigator({
+  //Drawer Optons and indexing
+  ViewEntries: { //Title
+    screen: StackNavigatorViewEntries,
+    navigationOptions: {
+      drawerLabel: "Home"
+    }
+  },
+  ViewSettings: { //Title
+    screen: StackNavigatorViewSettings,
+    navigationOptions: {
+      drawerLabel: "Settings"
+    }
+  },
+});
+export default createAppContainer(DrawerNavigator);
+
+
+// const MainApp = () =>{ 
+
+//   return (
+//     <ViewEntries />
+//   )
+// }
+
+// export default MainApp;
---
+++ components/ViewSettings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { View } from 'react-native'
+
+const ViewSettings = () => {
+
+
+  return (
+    <View>Settings</View>
+  )
+}
+
+export default ViewSettings;
